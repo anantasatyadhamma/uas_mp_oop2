550e0be7caac56b1cd89c23e41a778b7592cd646
anantasatyadhamma <satyadhammaananta@gmail.com>
2020-12-24 00:52:07 +0700

    Ananta Satya Dhamma, add feature delete hewan, and add list hewan to recyclerview, and add feature onClick in list

---
+++ app/src/main/java/com/ananta/uas_mp_oop2/HewanAdapter.kt
@@ -0,0 +1,46 @@
+package com.ananta.uas_mp_oop2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ananta.uas_mp_oop2.database.Hewan
+import kotlinx.android.synthetic.main.adapter_hewan.view.*
+
+class HewanAdapter (private val hewan: ArrayList<Hewan>, private val listener: onAdapterListener) :
+    RecyclerView.Adapter<HewanAdapter.HewanViewHolder>() {
+
+    class HewanViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+
+    fun setData(list: List<Hewan>){
+        hewan.clear()
+        hewan.addAll(list)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HewanViewHolder {
+        return HewanViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.adapter_hewan, parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: HewanViewHolder, position: Int) {
+        val h = hewan[position]
+        holder.view.text_nama.text = h.nama_hewan
+        holder.view.text_nama.setOnClickListener {
+            listener.onClick(h)
+        }
+        holder.view.icon_delete.setOnClickListener {
+            listener.onDelete(h)
+        }
+
+    }
+
+    override fun getItemCount() = hewan.size
+
+    interface onAdapterListener{
+        fun onClick(hewan: Hewan)
+        fun onDelete(hewan: Hewan)
+    }
+
+}
--- app/src/main/java/com/ananta/uas_mp_oop2/ListHewanActivity.kt
+++ app/src/main/java/com/ananta/uas_mp_oop2/ListHewanActivity.kt
@@ -3,25 +3,62 @@ package com.ananta.uas_mp_oop2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
 import com.ananta.uas_mp_oop2.database.AdopsiDB
+import com.ananta.uas_mp_oop2.database.Hewan
+import kotlinx.android.synthetic.main.activity_list_hewan.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ListHewanActivity : AppCompatActivity() {
 
     val db by lazy { AdopsiDB.getDatabase(this)}
+    lateinit var hewanAdapter: HewanAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_hewan)
+        setupRecyclerView()
+
     }
 
     override fun onStart() {
         super.onStart()
+        loadData()
+    }
+
+    fun loadData(){
         CoroutineScope(Dispatchers.IO).launch {
             val hewan =  db.hewanDao().getHewan()
             Log.d("ListHewanActivity", "respons : $hewan")
+            withContext(Dispatchers.Main){
+                hewanAdapter.setData( hewan )
+            }
+        }
+    }
+
+    private fun setupRecyclerView(){
+        hewanAdapter = HewanAdapter(arrayListOf(), object  : HewanAdapter.onAdapterListener{
+            override fun onClick(hewan: Hewan) {
+                Toast.makeText(applicationContext, hewan.nama_hewan + ", " + hewan.umur + ", " + hewan.owner, Toast.LENGTH_LONG).show()
+            }
+
+            override fun onDelete(hewan: Hewan) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    db.hewanDao().deleteHewan(hewan)
+                    loadData()
+                }
+            }
+
+        })
+        list_hewan.apply {
+            layoutManager = LinearLayoutManager(applicationContext)
+            adapter = hewanAdapter
         }
     }
 }
